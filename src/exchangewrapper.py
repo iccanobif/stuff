01fbf55612b931c4d2d3ac0a5182e9ec6c9c7f79
name <mail@mail.com>
2017-11-25 20:05:41 +0100

    Just assume that there's no missing candles in the backtesting dataset

--- src/exchangewrapper.py
+++ src/exchangewrapper.py
@@ -14,17 +14,20 @@ class ExchangeWrapperForBacktesting:
             self.marketName = marketName
             self.f = open(config.backtestingDataDirectory + "/" + marketName + ".json")
             line = self.f.readline()
-            self.previousTick = Candle(marketName, json.loads(line))
-            self.currTick = self.previousTick
+            self.previousTick = None
+            self.currTick = Candle(marketName, json.loads(line))
 
         def get(self, timestamp):
             
             if timestamp < self.currTick.getTimestamp():
-                return self.previousTick
+                if not self.previousTick is None:
+                    return self.previousTick
+                else:
+                    return self.currTick
             
             # If I'm asked for a timestamp later than the current position in 
             # the json, burn through the extra candles
-            while timestamp >= self.currTick.getTimestamp():
+            while timestamp > self.currTick.getTimestamp():
                 line = self.f.readline()
                 self.currTick = Candle(self.marketName, json.loads(line))
             
