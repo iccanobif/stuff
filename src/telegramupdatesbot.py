23469a24223349bfb86ac586051c564197132930
name <mail@mail.com>
2017-12-29 08:41:38 +0100

    Remove circular dependency between utilities.py and exchangewrapper.py

--- src/telegramupdatesbot.py
+++ src/telegramupdatesbot.py
@@ -8,16 +8,38 @@ from logger import Logger
 import config
 import utilities
 
+def getCurrentHoldings():
+    # Returns a formatted string
+    exchange = ExchangeWrapper()
+    balances = exchange.getBalances()
+    totalBtcValue = 0
+    output = ""
+    for currency in balances.keys():
+        btcValue = None
+        if currency == "BTC":
+            btcValue = balances[currency]
+        else:
+            btcValue = exchange.getCurrentTick("BTC-" + currency).price * balances[currency]
+        totalBtcValue += btcValue
+        output += "%f %s (%f BTC)\n" % (balances[currency], currency, btcValue)
+    output += "\nBTC total: %f (%f EURO)" % (totalBtcValue, totalBtcValue * getBTCPriceInEuro())
+    
+    return output
+
+def handleSummaryRequest(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Gathering data...")
+    bot.send_message(chat_id=update.message.chat_id, text=getCurrentHoldings())
+
 def main():
     try:
         Logger.open()
-        utilities.initializeTelegramBot()
+        utilities.initializeTelegramBot([("summary", handleSummaryRequest)])
 
         exchange = ExchangeWrapper()
 
         Logger.sendTelegramMessage("Bot started...")
         while True:
-            Logger.sendTelegramMessage(utilities.getCurrentHoldings())
+            Logger.sendTelegramMessage(getCurrentHoldings())
             time.sleep(60*10) # 10 minutes
         Logger.close()
     except:
--- src/utilities.py
+++ src/utilities.py
@@ -1,20 +1,15 @@
 import traceback, sys, time, logging, json
 import requests
 
-from analyst import Analyst
-from exchangewrapper import ExchangeWrapper
 from logger import Logger
-from marketstatusrepository import MarketStatusRepository
 import config
 from telegram.ext import Updater, CommandHandler
 
 telegramUpdater = None
 
-def handleSummaryRequest(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Gathering data...")
-    bot.send_message(chat_id=update.message.chat_id, text=getCurrentHoldings())
-
-def initializeTelegramBot():
+def initializeTelegramBot(handlerList):
+    # handlerList has to be a list of tuples with the name of the command to be typed 
+    # on the chat and the respective function to call (eg. [("summary", someHandler), ("buy", someOtherHandler)])
     global telegramUpdater
 
     if not config.enableTelegramIntegration:
@@ -24,28 +19,11 @@ def initializeTelegramBot():
     telegramUpdater = Updater(token='500739610:AAGhPu4Z5BNefe3_86NWqNutOQAV_Ywuzm0')
     dispatcher = telegramUpdater.dispatcher
     chatid = config.telegramChatId
-    dispatcher.add_handler(CommandHandler('summary', handleSummaryRequest))
+    for command in handlerList:
+        dispatcher.add_handler(CommandHandler(command[0], command[1]))
     telegramUpdater.start_polling()
     # updater.idle()
  
-def getCurrentHoldings():
-    # Returns a formatted string
-    exchange = ExchangeWrapper()
-    balances = exchange.getBalances()
-    totalBtcValue = 0
-    output = ""
-    for currency in balances.keys():
-        btcValue = None
-        if currency == "BTC":
-            btcValue = balances[currency]
-        else:
-            btcValue = exchange.getCurrentTick("BTC-" + currency).price * balances[currency]
-        totalBtcValue += btcValue
-        output += "%f %s (%f BTC)\n" % (balances[currency], currency, btcValue)
-    output += "\nBTC total: %f (%f EURO)" % (totalBtcValue, totalBtcValue * getBTCPriceInEuro())
-    
-    return output
-
 def isTelegramUpdaterActive():
     global telegramUpdater
     return telegramUpdater is not None
@@ -71,5 +49,3 @@ def prettyPrintJSONVertical(j):
 def prettyPrintJSONHorizontal(j):
     return json.dumps(j, sort_keys=True)
 
-if __name__ == "__main__":
-    print(getCurrentHoldings())
